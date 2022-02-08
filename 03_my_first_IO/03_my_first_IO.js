6b69060630cd9086536a356a167a004b62cd3ab1
Henry Johnson <jo74705@gmail.com>
2022-02-08 15:44:54 +0800

    Fix bug

--- 03_my_first_IO/03_my_first_IO.js
+++ 03_my_first_IO/03_my_first_IO.js
@@ -3,3 +3,4 @@ const argv = process.argv[2]
 fs.readFile(argv, 'utf-8', function callback(err, data){
 	if(err) console.err(err);
 	console.log(data.split('\n').lengh);
+})
--- learnyounode/IO.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let buffer = fs.readFileSync(process.argv[2]);
-const count = buffer.toString().split('\n').length - 1;
-console.log(count);
